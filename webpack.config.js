a8d6833b8d6fd730f88c260391eacb0be6ae165c
Einoff <einsteinov@gmail.com>
2021-07-29 11:33:12 +0400

    readme

--- webpack.config.js
+++ webpack.config.js
@@ -12,22 +12,15 @@ module.exports = {
     },
     devtool: 'inline-source-map',
     devServer: {
-        headers: {"Access-Control-Allow-Origin": "*"},
         open: true,
         port: 3000,
-        // https: true,
         historyApiFallback: {
             disableDotRule: true,
-        },
-        // headers: {
-        //     "Access-Control-Allow-Origin": "*",
-        //     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-        //     "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-        // }
+        }
     },
     resolve: {
         extensions: ['.js', '.jsx'],
-      },
+    },
     plugins: [
         new HtmlWebpackPlugin(
             {
